2f8e12b185e9babce4223f02bd3b0831a9c4cd68
Virendra Engole <45138342+virendraengole@users.noreply.github.com>
2025-07-21 10:01:19 +0530

    How do you find duplicate characters in a string
    How do you find duplicate characters in a string?

--- Test.java
+++ Test.java
@@ -1,51 +1,27 @@
-import java.io.*;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
-import javax.sound.midi.Soundbank;
+import java.util.Scanner;
 
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.hssf.usermodel.*;
-import org.apache.poi.ss.util.CellRangeAddress;
+//How do you find duplicate characters in a string?
 public class Test {  
-	//How do you count the number of occurrences of each character in a string?
-	//		Given a string , you have to count the number of occurrences of each character in it. 
-	//For example, If “Java J2EE Java JSP J2EE” is the given string then occurrences of 
-	//each character in this string is E=4, 2=2, v=2,  =4, P=1, S=1, a=4, J=5.
-			 
-			public static void main(String[] args) {
-			        String s ="Java J2EE Java JSP J2EE";
-			        s = s.replaceAll(" ", "");
-			        System.out.println("full string = "+s);
-			        Map<Character,Integer> m = new LinkedHashMap();
-			        for(Character c : s.toCharArray()){
-			          // System.out.println("Char = "+c);
-			           if(m.containsKey(c)) {
-			               m.put(c,m.get(c)+1);
-			           } else {
-			               m.put(c,1);
-			           }
-			        }
-			        for(Map.Entry<Character,Integer> ec : m.entrySet()) {
-			           System.out.println("Character Of = "+ec.getKey()+ " Char Count = "+ec.getValue()); 
-			        }
-			    }
 
-	
-	}
-class Main {
     public static void main(String[] args) {
-        String s = "Java J2EE Java JSP J2EE";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string : ");
+        String s = sc.nextLine();
+        m1(s);
+        }
+    public static void m1(String s) {
         int[] n = new int[256];
-        for(int i = 0; i< s.length(); i++) {
-            if(' ' != s.charAt(i)) {
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) != ' ') {
                 n[s.charAt(i)]++;
             }
         }
-        for(int j = 0 ; j < 256; j++) {
+        for(int j = 0; j < n.length; j++) {
             if(n[j] > 0) {
-                System.out.println((char)j+" char count "+n[j]);
+            System.out.println((char) j + " char of duplicate " +n[j]);
             }
         }
     }
+
 }
